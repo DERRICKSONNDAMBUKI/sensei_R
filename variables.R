7c47d41dcfdc8ee4b135f65d30f64566aaf15025
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-19 00:57:55 +0300

    R commit

--- variables.R
+++ variables.R
@@ -12,3 +12,14 @@ print(var.1)
 cat("var.1 is ",var.1,"\n")
 cat("var.2 is ",var.2,"\n")
 cat("var.3 is ",var.3,"\n")
+
+# finding variables
+print(ls())
+# list the variables starting woth pattern "var" using ls()
+print(ls(pattern = ".2"))
+# The variables starting with dot(.) are hidden, they can be listed using "all.names = TRUE" argument to ls() function.
+# print(all.name = TRUE)
+
+# deleting variables using rm() and ls()
+rm(list = ls())
+print(ls())
