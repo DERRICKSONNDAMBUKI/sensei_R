43b31bfaee768bb7c093911f5a8c90c1a51110e6
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-19 12:34:06 +0300

    strings commit

--- vectors.R
+++ vectors.R
@@ -5,3 +5,21 @@ print(apple)
 
 # Get the class of the vector
 print(class(apple))
+
+# Atomic vector of type character.
+print("abc");
+
+# Atomic vector of type double.
+print(12.5)
+
+# Atomic vector of type integer.
+print(63L)
+
+# Atomic vector of type logical.
+print(TRUE)
+
+# Atomic vector of type complex.
+print(2+3i)
+
+# Atomic vector of type raw.
+print(charToRaw('hello'))
