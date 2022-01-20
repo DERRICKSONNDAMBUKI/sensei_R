beb6e72e1b2678f06d5f892f4718d7c3cc6668a2
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-20 12:13:34 +0300

    matrices commit

--- matrices.R
+++ matrices.R
@@ -1,3 +1,18 @@
 # create a matrix - are only 2D with any number of rows
 M = matrix(c('a','a','b','c','b','a'),nrow = 3,byrow = TRUE)
 print(M)
+
+# Elements are arranged sequentially by row.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
+# Elements are arranged sequentially by column.
+N <- matrix(c(3:14), nrow = 4, byrow = FALSE)
+print(N)
+
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+print(P)
--- vectors.R
+++ vectors.R
@@ -23,3 +23,43 @@ print(2+3i)
 
 # Atomic vector of type raw.
 print(charToRaw('hello'))
+
+# Create two vectors.
+v1 <- c(3,8,4,5,0,11)
+v2 <- c(4,11,0,8,1,2)
+
+# Vector addition.
+add.result <- v1+v2
+print(add.result)
+
+# Vector substraction.
+sub.result <- v1-v2
+print(sub.result)
+
+# Vector multiplication.
+multi.result <- v1*v2
+print(multi.result)
+
+# Vector division.
+divi.result <- v1/v2
+print(divi.result)
+
+# vector element sorting
+v <- c(3,8,4,5,0,11, -9, 304)
+
+# Sort the elements of the vector.
+sort.result <- sort(v)
+print(sort.result)
+
+# Sort the elements in the reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
+
+# Sorting character vectors.
+v <- c("Red","Blue","yellow","violet")
+sort.result <- sort(v)
+print(sort.result)
+
+# Sorting character vectors in reverse order.
+revsort.result <- sort(v, decreasing = TRUE)
+print(revsort.result)
