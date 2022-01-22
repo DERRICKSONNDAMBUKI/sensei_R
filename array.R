7b88e26595cbb4f1f1d13274d77b831316ce0a4a
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-22 12:52:50 +0300

    arrays commit

--- array.R
+++
@@ -1,4 +0,0 @@
-# create an array - While matrices are confined to two dimensions, arrays can be of any number of dimensions. 
-a<-array(c('green','yellow'),dim =c(3,3,2))
-# The array function takes a dim attribute which creates the required number of dimension.
-print(a)
