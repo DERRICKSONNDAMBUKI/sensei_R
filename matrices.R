63744180dfd41fa8855cd6290a8d86593d8d0551
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-22 12:44:15 +0300

    accessing matrices commit

--- matrices.R
+++ matrices.R
@@ -16,3 +16,15 @@ colnames = c("col1", "col2", "col3")
 
 P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
 print(P)
+
+# Access the element at 3rd column and 1st row.
+print(P[1,3])
+
+# Access the element at 2nd column and 4th row.
+print(P[4,2])
+
+# Access only the  2nd row.
+print(P[2,])
+
+# Access only the 3rd column.
+print(P[,3])
