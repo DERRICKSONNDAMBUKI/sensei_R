16f5e96c36da9e23f5a5cf30eb8a498360864573
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-23 14:44:17 +0300

    csv commit

--- reading_csv.R
+++ reading_csv.R
@@ -5,3 +5,18 @@ print(data)
 print(is.data.frame(data))
 print(ncol(data))
 print(nrow(data))
+
+# getting maximum salary
+maxSalary <-max(data$salary)
+
+# get the person detail having max salary
+retval <- subset(data, salary == max(maxSalary))
+print(retval)
+
+# get persons in IT department
+retval <- subset( data, dept == "IT")
+print(retval)
+
+# get persons in IT department and salry is greater than 600
+info <- subset(data, salary > 600 & dept == "IT")
+print(info)
