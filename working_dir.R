1f7e5c720aa81f272e12c731605858bc14ad2c15
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-23 13:10:57 +0300

    csv commit

---
+++ working_dir.R
@@ -0,0 +1,11 @@
+# Getting and Setting the Working Directory
+# You can check which directory the R workspace is pointing to using the getwd() function. You can also set a new working directory using setwd()function.
+
+# Get and print current working directory.
+print(getwd())
+
+# Set current working directory.
+# setwd("/web/com")
+
+# Get and print current working directory.
+print(getwd())
