7eb02881e44f176a13246d665defa3de3a8c5d34
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-23 15:10:31 +0300

    xlsx commit

---
+++ read_excel.R
@@ -0,0 +1,3 @@
+# Read the first worksheet in the file input.xlsx.
+data <- read.xlsx("input.xlsx", sheetIndex = 1)
+print(data)
---
+++ verify_xlsx.R
@@ -0,0 +1,7 @@
+# verify the package is installed
+any(grepl("xlsx",installed.packages()))
+
+# load the library in R workspace
+library("xlsx")
+
+# ricky has bugs
