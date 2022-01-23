a88f9b74eaa4b7088f2740c947a3ef8c5f2c7f8c
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-23 20:04:44 +0300

    bar chart commit

---
+++ bar_charts.R
@@ -0,0 +1,21 @@
+# The basic syntax to create a bar-chart in R is:
+# barplot(H,xlab,ylab,main, names.arg,col)
+# Following is the description of the parameters used :
+    # H is a vector or matrix containing numeric values used in bar chart.
+    # xlab is the label for x axis.
+    # ylab is the label for y axis.
+    # main is the title of the bar chart.
+    # names.arg is a vector of names appearing under each bar.
+    # col is used to give colors to the bars in the graph.
+
+# create the data for the chart
+H<-c(7,12,28,3,41)
+
+# give the chart file name
+png(file = "./files/barchart.png")
+
+# plot the chart
+barplot(H)
+
+# save the file
+dev.off()
