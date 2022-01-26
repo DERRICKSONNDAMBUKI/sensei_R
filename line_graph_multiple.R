f218c0b1ef1e995b66dc8495423d15547ea9da28
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-27 01:52:54 +0300

    line graph multiple lines commit

---
+++ line_graph_multiple.R
@@ -0,0 +1,12 @@
+# Create the data for the chart.
+v <- c(7,12,28,3,41)
+t <- c(14,7,6,19,3)
+
+# Give the chart file a name.
+png(file = "line_chart_2_lines.jpg")
+
+# plot the line graph
+plot(v, type ="o",, col = "red",xlab = "Month",ylab = "Rain Fall",main = "Rain Fall Chart")
+
+lines(t,type="o" ,col="blue")
+dev.off()
