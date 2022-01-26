ed8014f7d16a7ffcc6037c404ab59463fbbed8cb
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-27 02:08:07 +0300

    scatter plot matrices commit

---
+++ scatterplot_matrices.R
@@ -0,0 +1,22 @@
+# When we have more than two variables and we want to find the correlation between one variable versus the remaining ones we use scatterplot matrix. We use pairs() function to create matrices of scatterplots.
+
+# pairs(formula, data)
+
+# Following is the description of the parameters used −
+    # formula represents the series of variables used in pairs.
+    # data represents the data set from which the variables will be taken.
+
+# Get the input values.
+input <- mtcars[,c('wt','mpg')]
+
+# Give the chart file a name.
+png(file = "scatterplot_matrices.png")
+
+# Plot the matrices between 4 variables giving 12 plots.
+
+# One variable with 3 others and total 4 variables.
+
+pairs(~wt+mpg+disp+cyl,data=mtcars,main = "Scatterplot Matrix")
+
+# save the file
+dev.off()
