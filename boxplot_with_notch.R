ba5a489c95b2f202d7cf6b15535644125f52bfb5
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-27 01:22:46 +0300

    boxplot with notch commit

---
+++ boxplot_with_notch.R
@@ -0,0 +1,21 @@
+input <- mtcars[,c('mpg','cyl')]
+print(head(input))
+
+# give the chart file a name
+png(file ="./files/boxplot_with_notch.png")
+
+# Plot the chart
+boxplot(
+    mpg~cyl,
+    data = mtcars,
+    xlab = "Number of Cylinders",
+    ylab = "Miles per Gallon",
+    main = "Mileage Data",
+    notch = TRUE,
+    varwidth = TRUE,
+    col = c("green","yellow","purple"),
+    names = c("High","Medium","Low")
+)
+
+# save the file
+dev.off
