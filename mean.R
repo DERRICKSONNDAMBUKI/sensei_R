f9fe93696005cc58c4eb75325ac0a2a0d5dcd909
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-28 11:54:54 +0300

    mean commit

---
+++ mean.R
@@ -0,0 +1,13 @@
+# mean(x, trim = 0, na.rm = FALSE, ...)
+
+# Following is the description of the parameters used −
+    # x is the input vector.
+    # trim is used to drop some observations from both end of the sorted vector.
+    # na.rm is used to remove the missing values from the input vector.
+
+# Create a vector. 
+x <- c(12,7,3,4.2,18,2,54,-21,8,-5)
+
+# Find Mean.
+result.mean <- mean(x)
+print(result.mean)
