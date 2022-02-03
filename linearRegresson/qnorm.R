ed4522364f24c62e83b88ede99ffbf2cc25ae409
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-03 23:57:51 +0300

    qnorm() commit

--- linearRegresson/qnorm.R
+++ linearRegresson/qnorm.R
@@ -1,3 +1,18 @@
 # qnorm()
 
 # This function takes the probability value and gives a number whose cumulative value matches the probability value.
+
+# Create a sequence of probability values incrementing by 0.02.
+x <- seq(0, 1, by = 0.02)
+
+# Choose the mean as 2 and standard deviation as 3.
+y <- qnorm(x, mean = 2, sd = 1)
+
+# Give the chart file a name.
+png(file = "qnorm.png")
+
+# Plot the graph.
+plot(x,y)
+
+# Save the file.
+dev.off()
