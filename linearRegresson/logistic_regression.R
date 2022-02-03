873c14d80b1b4b50c57fdc597769ef36fac2fb1f
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-03 23:37:45 +0300

    Logistic regression commit

--- linearRegresson/logistic_regression.R
+++ linearRegresson/logistic_regression.R
@@ -5,3 +5,9 @@
     # x is the predictor variable.
     # a and b are the coefficients which are numeric constants.
 # The function used to create the regression model is the glm() function.
+
+# The in-built data set "mtcars" describes different models of a car with their various engine specifications. In "mtcars" data set, the transmission mode (automatic or manual) is described by the column am which is a binary value (0 or 1). We can create a logistic regression model between the columns "am" and 3 other columns - hp, wt and cyl.
+
+# select some columns form mtcars.
+input <- mtcars[,c("am","cyl","hp","wt")]
+print(head(input))
