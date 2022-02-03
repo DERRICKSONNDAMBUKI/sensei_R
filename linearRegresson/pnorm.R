affe9d0505e77f2dc6bd5884bdb6e06b3d90dc4b
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-03 23:54:36 +0300

    pnorm() commit

---
+++ linearRegresson/pnorm.R
@@ -0,0 +1,3 @@
+# pnorm()
+
+# This function gives the probability of a normally distributed random number to be less that the value of a given number. It is also called "Cumulative Distribution Function".
