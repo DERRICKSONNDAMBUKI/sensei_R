15e5718d0b6e3017af076c64e38b9a6eaf4a1670
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-03 23:35:06 +0300

    Logistic regression commit

---
+++ linearRegresson/logistic_regression.R
@@ -0,0 +1,7 @@
+# y = 1/(1+e^-(a+b1x1+b2x2+b3x3+...))
+
+# Following is the description of the parameters used −
+    # y is the response variable.
+    # x is the predictor variable.
+    # a and b are the coefficients which are numeric constants.
+# The function used to create the regression model is the glm() function.
