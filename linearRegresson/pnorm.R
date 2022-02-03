64063a09b603d19c82b6691d601061d037c19e3e
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-03 23:55:49 +0300

    pnorm() commit

--- linearRegresson/pnorm.R
+++ linearRegresson/pnorm.R
@@ -1,3 +1,18 @@
 # pnorm()
 
 # This function gives the probability of a normally distributed random number to be less that the value of a given number. It is also called "Cumulative Distribution Function".
+
+# Create a sequence of numbers between -10 and 10 incrementing by 0.2.
+x <- seq(-10,10,by = .2)
+ 
+# Choose the mean as 2.5 and standard deviation as 2. 
+y <- pnorm(x, mean = 2.5, sd = 2)
+
+# Give the chart file a name.
+png(file = "file/pnorm.png")
+
+# Plot the graph.
+plot(x,y)
+
+# Save the file.
+dev.off()
